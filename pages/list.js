069e9924a9314a8418f4c930abcaf446675802e7
kgonjo <kgonjo@yahoo-corp.jp>
2020-05-16 08:46:32 +0900

    Forgot Add

---
+++ pages/list.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const List = () => {
+  return (
+    <Layout>
+      <p>List Page</p>
+    </Layout>
+  )
+}
+
+export default List
